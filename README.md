d6ccc1ce93d4656028ddccef2a63090cc85a721c
Amanda Nelson <amandaln4311@gmail.com>
2021-01-07 19:50:55 -0500

    readme done

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Weather_Dashboard
+## Description
+This weather dashboard with give you current weather, UV Index, and 5 day forecast. The cities you search for will save under the search bar in listed order.
+
+*https://amandaln.github.io/Weather_Dashboard/
+
+## Technologies
+HTML
+
+CSS/Bootstrap
+
+JavaScript/JQuery
+
+Web APIs
+
+## Usage
+![Weather Dashboard screenshot](./assets/images/weather.png)
+
+## Maintainer
+Amanda Nelson
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- logic.js
+++ logic.js
@@ -5,21 +5,20 @@ $(document).ready(function () {
     let searchValue;
     let tRow = $("#today");
     // let UVIndex;
-    // let city;
-    // let cityHistory = $(".history");
-     let underRow = $("#forecast");
-    // let underRowDisplay = [];
+    // let underRow = $("#forecast");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    
 
     $("#search-button").on("click", function () {
         searchValue = $("#search-value").val();
         $("#search-value").val("")
         searchWeather(searchValue);
         searchHistory.push(searchValue);
-        localStorage.setItem("search",JSON.stringify(searchHistory));
+        localStorage.setItem("search", JSON.stringify(searchHistory));
         renderSearchHistory();
     })
 
-    $("#clear-history").on("click",function() {
+    $("#clear-history").on("click", function () {
         searchHistory = [];
         renderSearchHistory();
         localStorage.clear();
@@ -27,21 +26,21 @@ $(document).ready(function () {
 
     function renderSearchHistory() {
         $("#history").text("");
-        for (let i=0; i<searchHistory.length; i++) {
+        for (let i = 0; i < searchHistory.length; i++) {
             const historyItem = $("<button>");
-            historyItem.text(seeachHistory[i]);
-            historyItem.attr("readonly",true);
+            historyItem.text(searchHistory[i]);
+            historyItem.attr("readonly", true);
             historyItem.attr("class", "form-control d-block bg-white");
             historyItem.attr("value", searchHistory[i]);
-            historyItem.on("click",function() {
-                getWeather(historyItem).val();
+            historyItem.on("click", function () {
+                getWeather(historyItem.val());
             })
             $("#history").append(historyItem);
         }
     }
     renderSearchHistory();
     if (searchHistory.length > 0) {
-        createRow(searchHistory[searchHistory.length - 1]);
+        firstRow(searchHistory[searchHistory.length - 1]);
     }
 
 
@@ -76,67 +75,90 @@ $(document).ready(function () {
             console.log(UVIndex);
         })
 
-        // function forecast(response) {
-            
-            $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIKey,
-                method: "GET"
-            }).then(function (response) {
-                for (i = 0; i < 5; i++) {
-                    var date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
-                    var iconcode = response.list.weather[0].icon;
-                    var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
-                    var tempK = response.list.main.temp;
-                    var tempF = (((tempK - 273.5) * 1.80) + 32).toFixed(2);
-                    var humidity = response.list.main.humidity;
-    
-                    // $("#fDate" + i).html(date);
-                    // $("#fImg" + i).html("<img src=" + iconurl + ">");
-                    // $("#fTemp" + i).html(tempF + "&#8457");
-                    // $("#fHumidity" + i).html(humidity + "%");
-
-                    underRow.append(date, iconcode, iconurl, tempF, humidity);
-                // add row to body
-                    $("tbody").append(underRow);
-                };
-            });
         
 
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&units=imperial" + "&appid=" + APIkey,
+            method: "GET"
+        }).then(function (response) {
+            $("#fiveFor").empty();
+            let fiveFor = $("<h3>").text("5 Day Forecast");
+            $("#day1").empty();
+            $("#fiveFor").append(fiveFor);
+            // Creating date in forecast
+            var forDate = $("<div>").text(moment().add(1, 'days').format('MMMM Do YYYY'));
+            $("#day1").append(forDate)
+            var forIcon = $("<img>");
+            forIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[4].weather[0].icon+ ".png")
+            $("#day1").append(forIcon);
+            var forTemp = $("<div>").text("Temperature: " + response.list[4].main.temp + "°F")
+            $("#day1").append(forTemp)
+            var forHum = $("<div>").text("Humidity: " + response.list[4].main.humidity + "%")
+            $("#day1").append(forHum)
 
+            $("#day2").empty();
+            // Creating date in forecast
+            var forDate = $("<div>").text(moment().add(2, 'days').format('MMMM Do YYYY'));
+            $("#day2").append(forDate)
+            var forIcon = $("<img>");
+            forIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[12].weather[0].icon+ ".png")
+            $("#day2").append(forIcon);
+            var forTemp = $("<div>").text("Temperature: " + response.list[12].main.temp + "°F")
+            $("#day2").append(forTemp)
+            var forHum = $("<div>").text("Humidity: " + response.list[12].main.humidity + "%")
+            $("#day2").append(forHum)
 
+            $("#day3").empty();
+            // Creating date in forecast
+            var forDate = $("<div>").text(moment().add(3, 'days').format('MMMM Do YYYY'));
+            $("#day3").append(forDate)
+            var forIcon = $("<img>");
+            forIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[20].weather[0].icon+ ".png")
+            $("#day3").append(forIcon);
+            var forTemp = $("<div>").text("Temperature: " + response.list[20].main.temp + "°F")
+            $("#day3").append(forTemp)
+            var forHum = $("<div>").text("Humidity: " + response.list[20].main.humidity + "%")
+            $("#day3").append(forHum)
+
+            $("#day4").empty();
+            // Creating date in forecast
+            var forDate = $("<div>").text(moment().add(4, 'days').format('MMMM Do YYYY'));
+            $("#day4").append(forDate)
+            var forIcon = $("<img>");
+            forIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[28].weather[0].icon+ ".png")
+            $("#day4").append(forIcon);
+            var forTemp = $("<div>").text("Temperature: " + response.list[28].main.temp + "°F")
+            $("#day4").append(forTemp)
+            var forHum = $("<div>").text("Humidity: " + response.list[28].main.humidity + "%")
+            $("#day4").append(forHum)
+
+            $("#day5").empty();
+            // Creating date in forecast
+            var forDate = $("<div>").text(moment().add(5, 'days').format('MMMM Do YYYY'));
+            $("#day5").append(forDate)
+            var forIcon = $("<img>");
+            forIcon.attr("src", "http://openweathermap.org/img/wn/" + response.list[36].weather[0].icon+ ".png")
+            $("#day5").append(forIcon);
+            var forTemp = $("<div>").text("Temperature: " + response.list[36].main.temp + "°F")
+            $("#day5").append(forTemp)
+            var forHum = $("<div>").text("Humidity: " + response.list[36].main.humidity + "%")
+            $("#day5").append(forHum)
+        
+            
+        })
+    
     }
 
-    function searchWeather(searchValue) {
-        $.ajax({
+     function searchWeather(searchValue) {
+            $.ajax({
             method: "GET",
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial" + "&appid=" + APIkey,
 
-        }).then(function (response) {
+            }).then(function (response) {
             console.log(response);
             firstRow(response);
 
-        })
-    };
+            })
+        } 
 
-   
 })
-    // function forecastWeather(searchValue) {
-    //     $.ajax({
-    //      method: "GET",
-    //      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&units=imperial" + "&appid=" + APIkey,
-
-    //      }).then(function(response){
-    //             console.log(response);
-
-    //         $("day1").empty();
-    //         let forecastDate = $("<dvi>").text(moment().add(1, 'days').format('MMMM Do YYYY'));
-
-    //     })
-
-    //  
-    //  
-    // var dayover= false;
-
-
-
-    // }
